55e8e6ade501455758942f09d9798ca2e93e3013
belgaid-ansar <belgaid.ansar.info@gmail.com>
2022-03-23 16:16:17 +0100

    Ajout serveur et fichier config

---
+++ serveur.py
@@ -0,0 +1,39 @@
+import socket
+import threading
+import sys
+
+def traiter_client(sock_file):
+    while True:
+        mess = sock_file.recv(256)
+        if mess.decode() == "" :
+            break
+        sock_file.sendall(mess.upper())
+
+
+#Arguments
+if len(sys.argv) != 2:
+	print(f"Usage: {sys.argv[0]} <port>", file=sys.stderr)
+	sys.exit(1)
+
+sock_locale= socket.socket()
+sock_locale.bind(("", int(sys.argv[1])))
+sock_locale.listen(4)
+
+print("Serveur en attente sur le port " + sys.argv[1], file=sys.stderr)
+
+while True:
+    try:
+        sock_client, adr_client = sock_locale.accept()
+        threading.Thread(target=traiter_client, args = (sock_client, )).start()
+    except KeyboardInterrupt:
+        break
+
+sock_locale.shutdown(socket.SHUT_RDWR)
+print("Bye")
+
+for t in threading.enumerate():
+    if t != threading.main_thread(): t.join
+
+
+print("Arret du serveur", file=sys.stderr)
+sys.exit(0)
