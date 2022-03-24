b66bb18d992812da3fc37d21d74c01c0f5de7119
belgaid-ansar <belgaid.ansar.info@gmail.com>
2022-03-24 18:26:35 +0100

    modification du 24/03

--- serveur.py
+++ serveur.py
@@ -1,13 +1,41 @@
 import socket
 import threading
 import sys
+import re
 
-def traiter_client(sock_file):
+Users = {}
+nb_user=0
+
+def traiter_client(sock_file, adr_client, nb_user):
     while True:
+        print("Serveur à l'écoute")
+        #print(Users)
         mess = sock_file.recv(256)
         if mess.decode() == "" :
             break
-        sock_file.sendall(mess.upper())
+        else:
+            #on traite la commande CONNECT
+            if re.findall("CONNECT", mess.decode()):
+                x = mess.decode().split(" ")
+                print(x[1],"connected to the server")
+                retour_connection="200"
+                sock_file.sendall(retour_connection.encode()) # je renvoie le code de retour 
+                
+                Users[nb_user] = {}
+                Users[nb_user]['username'] = x[1]
+                Users[nb_user]['port'] = adr_client
+                
+                #On traite les autres commandes apres la commande CONNECT
+                if re.findall("CONNECT", mess.decode()):
+                    retour="418"
+                sock_file.sendall(retour.encode())
+            '''
+            elif mess.decode()=="QUIT":
+                sock_locale.shutdown(socket.SHUT_RDWR)
+                print("Bye")
+            '''
+
+        #sock_file.sendall(mess.upper())
 
 
 #Arguments
@@ -24,7 +52,8 @@ print("Serveur en attente sur le port " + sys.argv[1], file=sys.stderr)
 while True:
     try:
         sock_client, adr_client = sock_locale.accept()
-        threading.Thread(target=traiter_client, args = (sock_client, )).start()
+        nb_user =nb_user+1
+        threading.Thread(target=traiter_client, args = (sock_client, adr_client,nb_user)).start()
     except KeyboardInterrupt:
         break
 
@@ -32,8 +61,8 @@ sock_locale.shutdown(socket.SHUT_RDWR)
 print("Bye")
 
 for t in threading.enumerate():
-    if t != threading.main_thread(): t.join
+    if t != threading.main_thread(): 
+        t.join
+        print("Arret du serveur", file=sys.stderr)
 
-
-print("Arret du serveur", file=sys.stderr)
 sys.exit(0)
