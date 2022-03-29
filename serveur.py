3025c52146e745f9d5a966f5cf351b08d9003b2b
belgaid-ansar <belgaid.ansar.info@gmail.com>
2022-03-29 17:26:56 +0200

    Update CONNECT

--- serveur.py
+++ serveur.py
@@ -14,26 +14,35 @@ def valide_commande(commande):
 
 # METHODE traiter_client
 def traiter_client(sock_file, adr_client, nb_user):
+    CONNECTED = False
 
     #Traitement de la commande CONNECT
     print("Serveur à l'écoute")
     mess = sock_file.recv(256)
-    while not re.match(r"CONNECT [a-zA-Z]+", mess.decode()): #Tant qu'il n'est pas connecté
-        sock_file.sendall("402".encode())
-        mess = sock_file.recv(256)
+    while not CONNECTED :
+        while not re.match(r"CONNECT .+", mess.decode()): #Tant qu'il n'est pas connecté
+            sock_file.sendall("402".encode())
+            mess = sock_file.recv(256)
 
-    x = mess.decode().split(" ") #On recupere le username
+        while not re.match(r"CONNECT [a-zA-Z]+", mess.decode()): #Tant qu'il n'est pas connecté
+            sock_file.sendall("407".encode())
+            mess = sock_file.recv(256) 
 
-    #Verification du username
-    #.......
+        x = mess.decode().split(" ") #On recupere le username
+        print("bonjour")
+        #Verification du username
+        if x[1] in Users :
+            sock_file.sendall("406".encode())
+            mess = sock_file.recv(256)
+        else :
+            print(x[1],"connected to the server")
+            sock_file.sendall("200".encode()) # je renvoie le code de retour 
 
-    print(x[1],"connected to the server")
-    sock_file.sendall("200".encode()) # je renvoie le code de retour 
-    
-    #Creation de la liste des utilisateurs
-    Users[nb_user] = {}
-    Users[nb_user]['username'] = x[1]
-    Users[nb_user]['port'] = adr_client
+            #Ajout de l'utilisateur
+            Users[x[1]]={}
+            Users[x[1]]['port']= adr_client
+       
+            CONNECTED = True
 
     while True:
         print("Serveur à l'écoute")
