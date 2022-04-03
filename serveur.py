1f88d3a758215314f4908e3e182ee9af1b29b501
belgaid-ansar <belgaid.ansar.info@gmail.com>
2022-04-03 04:30:22 +0200

    update 03/04/2022

--- serveur.py
+++ serveur.py
@@ -7,94 +7,102 @@ import re
 Users = {}
 
 #METHODE valide_commande
-def valide_commande(commande):
-    if commande :
-        pass
+def match_commande(socket,commande):
+    if re.match(r"CONNECT .+", commande) or  re.match(r"USERS", commande) or  re.match(r"RENAME .+", commande) :
+        return True
+    else :
+        socket.sendall("402".encode())
+        return False
+#FIN METHODE
+
+#METHODE CONNECTED:
+def connect(socket,message): 
+    
+    if not re.match(r"CONNECT [a-zA-Z]+", message):  # On verifie l'orthographe du username
+        socket.sendall("407".encode())
+        return ""
+    else:
+        x = message.split(" ")  # On recupere le username
+        username = x[1]
+        
+        if username in Users:   # Verification du username
+            socket.sendall("406".encode())
+            return ""
+        else:
+            print(username, "connected to the server")
+            #io.emit('message', "this is a test");
+            socket.sendall("200".encode())  # je renvoie le code de retour
+
+            Users[username] = {}  # Ajout de l'utilisateur
+            Users[username]['port'] = socket
+            return username
+            
+#FIN METHODE
+
+#METHODE RENAME:
+def rename(socket,message,username):
+
+    if not re.match(r"RENAME [a-zA-Z]+", message) :
+            socket.sendall("207".encode())
+            return ""
+    else :
+        y = message.split(" ") # On recupere le username
+        new_username=y[1]
+        
+        if new_username in Users :  # Verification du username
+            socket.sendall("406".encode())
+            return ""
+        else :
+            Users[new_username] = Users.pop(username)
+            socket.sendall("203".encode())
+
+            for value in Users.values() :   # prevenir les autres clients
+                if Users[new_username]['port'] != value['port'] :
+                    code = "CODE "+new_username
+                    value["port"].sendall(code.encode())
+
+        return new_username            
 #FIN METHODE
 
 # METHODE traiter_client
-def traiter_client(sock_file, adr_client,username_client=None):
-    CONNECTED = False
+def traiter_client(sock_file, adr_client,username_client=None): 
 
-    #Traitement de la commande CONNECT
     print("Serveur à l'écoute")
-    mess = sock_file.recv(256)
-    while not CONNECTED :
-        while not re.match(r"CONNECT .+", mess.decode()): #Tant qu'il n'est pas connecté
-            sock_file.sendall("402".encode())
-            mess = sock_file.recv(256)
-
-        while not re.match(r"CONNECT [a-zA-Z]+", mess.decode()): #Tant qu'il n'est pas connecté
-            sock_file.sendall("407".encode())
-            mess = sock_file.recv(256) 
-
-        x = mess.decode().split(" ") #On recupere le username
-        username= x[1]
-        #print("bonjour")
-        #Verification du username
-        if username in Users :
-            sock_file.sendall("406".encode())
-            mess = sock_file.recv(256)
-        else :
-            print(username,"connected to the server")
-            #io.emit('message', "this is a test");
-            sock_file.sendall("200".encode()) # je renvoie le code de retour 
-
-            #Ajout de l'utilisateur
-            Users[username]={}
-            Users[username]['port']= sock_file
-            username_client= username
-
-            CONNECTED = True
-
-    while True and CONNECTED == True:
+    message = sock_file.recv(256).decode()
+    
+    while not match_commande(sock_file,message) :
         print("Serveur à l'écoute")
-        mess = sock_file.recv(256)
+        message = sock_file.recv(256).decode()
+    
+    CONNECTED =False
+    while not CONNECTED:
+        username=connect(sock_file,message)
+        if username != "":
+            username_client = username
+            CONNECTED=True
+        else:
+            print("Serveur à l'écoute")
+            message = sock_file.recv(256).decode()
+        
+    while CONNECTED:  # On traite les autres commandes apres la commande CONNECT
 
-        #On traite les autres commandes apres la commande CONNECT      
-        if re.match(r"CONNECT [a-zA-Z]+", mess.decode()):
+        print("Serveur à l'écoute")
+        message = sock_file.recv(256).decode() 
+
+        while not match_commande(sock_file,message) :
+            print("Serveur à l'écoute")
+            message = sock_file.recv(256).decode()
+        
+        if re.match(r"CONNECT [a-zA-Z]+", message):
             sock_file.sendall("418".encode())
 
-        elif mess.decode() == "USERS":
+        elif message == "USERS":
             sock_file.sendall(("200 "+" , ".join(list(Users.keys()))).encode())
 
-        elif re.match(r"RENAME .+", mess.decode()):
-            if not re.match(r"RENAME [a-zA-Z]+", mess.decode()) :
-                sock_file.sendall("207".encode())
-            else :
-                y = mess.decode().split(" ") #On recupere le username
-                new_username=y[1]
-                #Verification du username
-                if new_username in Users :
-                    sock_file.sendall("406".encode())
-                else :
-                    Users[new_username] = Users.pop(username)
-                    username_client = new_username
-                    sock_file.sendall("203".encode())
-
-                    #prevenir les autres clients
-                    for value in Users.values() : 
-                        if Users[username_client]['port'] != value['port'] :
-                            code = "CODE "+new_username
-                            value["port"].sendall(code.encode())
-
-        elif mess.decode() == "QUIT" :
-            pass
-
-        elif mess.decode() == "" :
-            pass
-
-        else: 
-            sock_file.sendall("402".encode())
-            
-            '''
-            elif mess.decode()=="QUIT":
-                sock_locale.shutdown(socket.SHUT_RDWR)
-                print("Bye")
-            '''
-
-        #sock_file.sendall(mess.upper())
-
+        elif re.match(r"RENAME .+", message):
+            val=rename(sock_file,message,username_client)
+            if val != "" :
+                username_client = val       
 # FIN METHODE 
 
 #Arguments
